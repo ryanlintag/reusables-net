2a912ac9a822ea33875b0b07ef9a080d9711a8e3
Ryan Lintag <77764315+ryanlintag-who@users.noreply.github.com>
2024-03-08 13:10:33 +0800

    Adding Initial Test Report

---
+++ Reports/FastReport/FastReportLibrary/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FastReportLibrary
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddFastReportLibrary(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+            services.AddFastReport();
+            services.AddTransient<IWebReportGenerator, TestReportGenerator>();
+            return services;
+        }
+
+        public static WebApplication UseFastReportLibrary(this WebApplication app)
+        {
+            app.UseFastReport();
+            return app;
+        }
+    }
+}
