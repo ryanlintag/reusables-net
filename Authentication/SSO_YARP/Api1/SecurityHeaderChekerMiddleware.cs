876d2d3de26cd3368ddf44f5c535474fc9450223
Ryan Lintag <77764315+ryanlintag-who@users.noreply.github.com>
2024-04-07 01:13:07 +0800

    Added Claims Transformation that will pass claims from YARP app to API

---
+++ Authentication/SSO_YARP/Api1/SecurityHeaderChekerMiddleware.cs
@@ -0,0 +1,15 @@
+﻿namespace Api1
+{
+    public class SecurityHeaderChekerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public SecurityHeaderChekerMiddleware(RequestDelegate next) => _next = next;
+        public async Task Invoke(HttpContext context)
+        {
+            if (!(context.Request.Headers.ContainsKey("x-user-json") && context.Request.Headers.ContainsKey("x-api-key")))
+                context.Response.StatusCode = 400;
+            else
+                await _next.Invoke(context);
+        }
+    }
+}
